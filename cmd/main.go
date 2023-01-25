419459465f60c16947938bab26e23d3d5f2f99c6
nikolay <kovshik@inbox.ru>
2023-01-25 12:25:42 +0300

    working client with delete func

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	
 	"context"
 	proto "github.com/mrkovshik/grpc_vacancy_database/grpc/proto"
 	"google.golang.org/grpc"
@@ -16,32 +15,84 @@ import (
 
 	_ "github.com/lib/pq"
 )
-var mainMenu string = "\n*****************************\n - Если хотите посмотреть всю таблицу вакансий, наберите \"посмотреть\", \n - Если хотите найти вакансию по названию наберите \"найти\"\n - Если хотите добавить строку - наберите \"добавить\", \n - Если хотите выйти из программы, наберите \"выход\"\n*****************************\n"
-var searchQry string = " SELECT vacancies.id, vacancy_name, key_skills, salary, vacancy_desc, job_types.job_type FROM vacancies JOIN job_types ON vacancies.job_type = job_types.id WHERE vacancy_name ILIKE '%"
 
-
+var mainMenu string = "\n*****************************\n - Если хотите посмотреть всю таблицу вакансий, наберите \"посмотреть\", \n - Если хотите найти вакансию по названию наберите \"найти\"\n - Если хотите добавить строку - наберите \"добавить\", \n - Если хотите удалить вакансию из базы, наберите \"удалить\", \n - Если хотите выйти из программы, наберите \"выход\"\n*****************************\n"
 func main() {
 
-	err:=mainDialog()
-	if err!=nil{
+	err := mainDialog()
+	if err != nil {
 		fmt.Println(err)
 	}
-
-
-
 }
 
+func gprcConnect ()(proto.DBServerClient,context.Context,error){
+	ctx := context.TODO()
+	conn, err := grpc.Dial(":8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		fmt.Println("dial error")
+		return nil,ctx, err
+	}
+	// defer conn.Close()
+	client := proto.NewDBServerClient(conn)
+	return client,ctx, err
 
-
-type vacQuery struct {
-	ID int
-	vacName   string
-	keySkills string
-	vacDesc   string
-	salary    int
-	jobCode   int
-	jobType string
 }
+func deleteDialog () (int,bool) {
+	var vacId int
+	var err error
+	fmt.Println("Введите идентификационный номер вакансии, которую хотите удалить, либо наберите \"назад\" для выхода в предыдущее меню")
+	scanner := bufio.NewScanner(os.Stdin)
+	OuterLoop:
+	for {
+		fmt.Print("> ")
+		if scanner.Scan() {
+			if scanner.Text() == "назад" {
+				return 0, false
+			}
+			searchKey:= scanner.Text()
+			vacId, err = strconv.Atoi(searchKey)
+			if err != nil {
+				fmt.Println("Ошибка ввода данных (идентификационный номер должен быть числом), повторите ввод")
+				continue OuterLoop
+			}
+
+	fmt.Printf("Вы уверены, что хотите удалить из базы вакансию с номером %v? Наберите \"да\" или \"нет\"", vacId)
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("> ")
+		if scanner.Scan() {
+			if scanner.Text() == "да" {
+				break
+			}
+			if scanner.Text() == "нет" {
+				fmt.Println("Введите идентификационный номер вакансии, которую хотите удалить, либо наберите \"назад\" для выхода в предыдущее меню")
+				continue OuterLoop
+			}
+			fmt.Println("Наберите \"да\" или \"нет\"")
+			}
+		}
+	}
+
+			return vacId, true
+		}
+	}
+
+	func deleteVac (vacId int) (error) {
+			client,ctx,err:=gprcConnect()
+			if err != nil {
+				fmt.Println("RPC connect error")
+				return err
+			}
+			result, err := client.Delete(ctx, &proto.DeleteRequest{DeleteTarget: int32(vacId)})
+			if err != nil {
+				fmt.Println("RPC execution error")
+				return err
+			}
+			fmt.Println(result.DeleteResult)
+			return err
+		}
+	
+
 
 
 func searchDialog() (string, bool) {
@@ -59,51 +110,45 @@ func searchDialog() (string, bool) {
 	}
 }
 
-func loadVacs(qry string) [] vacQuery{
-		ctx := context.TODO()
-	conn, err := grpc.Dial("172.27.148.130:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+func loadVacs(qry string) ([]*proto.VacancyStruct, error) {
+client,ctx,err:=gprcConnect()
+if err != nil {
+	fmt.Println("RPC error")
+	return nil, err
+}
+	resp, err := client.Read(ctx, &proto.ReadRequest{ReadQuery: qry})
 	if err != nil {
-		panic("No calculus")
+		fmt.Println("RPC error")
+		return nil, err
 	}
-	defer conn.Close()
-	client := proto.NewDBServerClient(conn)
-	resp, err :=client.Get(ctx, &proto.GetRequest{Query: qry})
-result:= strings.Split(resp.String(), "\n")
-	
+	return resp.ReadResult, err
 }
 
-func showVacs(resSlice []vacQuery) error {
-var counter int
+func showVacs(resSlice []*proto.VacancyStruct) error {
+	fmt.Println("showing")
+	var counter int
 	var err error
 	const padding = 1
-
-
-	
-
-
-
-	
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, padding, ' ', tabwriter.AlignRight|tabwriter.Debug)
-	for i,line:= range resSlice {
-counter++
-			if i == 0 {
-			
-			_,err= fmt.Fprintln(w, "\tID\tНазвание вакансии\tКлючевые навыки\tОписание вакансии\tЗарплата\tТип работы\t")
+	for i, line := range resSlice {
+		counter++
+		if i == 0 {
+
+			_, err = fmt.Fprintln(w, "\tID\tНазвание вакансии\tКлючевые навыки\tОписание вакансии\tЗарплата\tТип работы\t")
 			if err != nil {
 				return err
 			}
-			_,err= fmt.Fprintln(w, "\t--\t-----------------\t------------------------------------------\t-----------------------------------------------------------------\t--------\t----------\t")
+			_, err = fmt.Fprintln(w, "\t--\t-----------------\t------------------------------------------\t-----------------------------------------------------------------\t--------\t----------\t")
 			if err != nil {
 				return err
 			}
 		}
-		
 
-		_,err= fmt.Fprintf(w, "\t%v\t%v\t%v\t%v\t%v\t%v\t\n",line.ID, line.vacName, line.keySkills, line.vacDesc, line.salary, line.jobType)
+		_, err = fmt.Fprintf(w, "\t%v\t%v\t%v\t%v\t%v\t%v\t\n", line.ID, line.VacName, line.KeySkills, line.VacDesc, line.Salary, line.JobType)
 		if err != nil {
 			return err
 		}
-		_,err= fmt.Fprintln(w, "\t--\t-----------------\t------------------------------------------\t-----------------------------------------------------------------\t--------\t----------\t")
+		_, err = fmt.Fprintln(w, "\t--\t-----------------\t------------------------------------------\t-----------------------------------------------------------------\t--------\t----------\t")
 		if err != nil {
 			return err
 		}
@@ -114,12 +159,11 @@ counter++
 		fmt.Println("----------------------------------------")
 		return err
 	}
-	
+
 	return err
 }
-func insertDialog() (vacQuery, bool) {
-	var result vacQuery
-	var err error
+func insertDialog() (proto.VacancyStruct, bool) {
+	var result proto.VacancyStruct
 	fmt.Println("введите соответствующие значения строк, разделяя их знаком \"/\": ")
 	fmt.Println("название вакансии, ключевые навыки, описание вакансии, зарплата, и код типа работы: 1 для работы в офисе, 2 для удаленной работы и 3 для гибридной формы работы")
 	fmt.Println("Например: \"Охранник/Решать конфликтные ситуации, обращаться с оружием, разгадывать сканворды/Человек, который следит за порядком в офисном здании/50000/1\"")
@@ -129,7 +173,7 @@ func insertDialog() (vacQuery, bool) {
 		fmt.Print("> ")
 		if scanner.Scan() {
 			if scanner.Text() == "назад" {
-				return vacQuery{}, false
+				return proto.VacancyStruct{}, false
 			}
 
 			queryString := strings.Split(scanner.Text(), "/")
@@ -137,20 +181,23 @@ func insertDialog() (vacQuery, bool) {
 				fmt.Println("Неверное количество аргументов, повторите ввод")
 				continue
 			}
-			result.vacName = queryString[0]
-			result.keySkills = queryString[1]
-			result.vacDesc = queryString[2]
-			result.salary, err = strconv.Atoi(queryString[3])
+			result.VacName = queryString[0]
+			result.KeySkills = queryString[1]
+			result.VacDesc = queryString[2]
+			intSal, err := strconv.Atoi(queryString[3])
+
 			if err != nil {
 				fmt.Println("Ошибка ввода данных в поле \"Зарплата\", повторите ввод")
 				continue
 			}
-			result.jobCode, err = strconv.Atoi(queryString[4])
+			result.Salary = int32(intSal)
+			intCode, err := strconv.Atoi(queryString[4])
 			if err != nil {
 				fmt.Println("Ошибка ввода данных в поле \"код типа работы\", повторите ввод")
 				continue
 			}
-			if result.jobCode > 3 || result.jobCode < 1 {
+			result.JobCode = int32(intCode)
+			if result.JobCode > 3 || result.JobCode < 1 {
 				fmt.Println("Код работы может быть только следующих значений: 1 для работы в офисе, 2 для удаленной работы и 3 для гибридной формы работы. Ввод других значений не допускается")
 				continue
 			}
@@ -160,17 +207,19 @@ func insertDialog() (vacQuery, bool) {
 
 	}
 }
-func insert(q vacQuery) error {
-	ctx := context.TODO()
-	conn, err := grpc.Dial("172.27.148.130:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+func insert(q proto.VacancyStruct) error {
+	client,ctx,err:=gprcConnect()
 	if err != nil {
-		panic("GRPC connection error")
+		fmt.Println("RPC error")
+		return err
 	}
-	defer conn.Close()
-	client := proto.NewDBServerClient(conn)
-	result, err :=client.Put(ctx, &proto.PutRequest{VacName: q.vacName, KeySkills: q.keySkills, VacDesc:q.vacDesc, Salary: int32(q.salary), JobCode: int32(q.jobCode)})
-	fmt.Println(result)
+	result, err := client.Insert(ctx, &proto.InsertRequest{NewVac: &proto.VacancyStruct{VacName: q.VacName, KeySkills: q.KeySkills, VacDesc: q.VacDesc, Salary: int32(q.Salary), JobCode: int32(q.JobCode)}})
+	if err != nil {
 
+		return err
+	}
+	fmt.Println(result)
+	return nil
 }
 
 func mainDialog() error {
@@ -185,30 +234,30 @@ OuterLoop:
 		if scanner.Scan() {
 			switch {
 			case scanner.Text() == "посмотреть":
-				res,err := loadVacs(searchQry+"%'")
+				res, err := loadVacs("")
 				if err != nil {
-					fmt.Println("Ошибка обращения к базе данных", err)
+					fmt.Println("Ошибка обращения к серверу", err)
 					return err
 				}
-				err=showVacs(res)
+				err = showVacs(res)
 				if err != nil {
-					fmt.Println("Ошибка обращения к базе данных", err)
+					fmt.Println("Ошибка обращения к серверу", err)
 					return err
 				}
 				fmt.Println(mainMenu)
 			case scanner.Text() == "найти":
 				keyWord, proceed := searchDialog()
 				if proceed {
-					res,err := loadVacs(searchQry+keyWord+"%'")
-				if err != nil {
-					fmt.Println("Ошибка обращения к базе данных", err)
-					return err
-				}
-				err=showVacs(res)
-				if err != nil {
-					fmt.Println("Ошибка обращения к базе данных", err)
-					return err
-				}
+					res, err := loadVacs(keyWord)
+					if err != nil {
+						fmt.Println("Ошибка обращения к серверу", err)
+						return err
+					}
+					err = showVacs(res)
+					if err != nil {
+						fmt.Println("Ошибка обращения к серверу", err)
+						return err
+					}
 				}
 				fmt.Println(mainMenu)
 			case scanner.Text() == "добавить":
@@ -217,6 +266,16 @@ OuterLoop:
 					err := insert(query)
 					if err != nil {
 						fmt.Println("Ошибка внесения данных в таблицу, попробуйте еще раз", err)
+						return err
+					}
+				}
+				fmt.Println(mainMenu)
+			case scanner.Text() == "удалить":
+				query, proceed := deleteDialog()
+				if proceed {
+					err := deleteVac(query)
+					if err != nil {
+						fmt.Println("Ошибка удаления данных в таблицу, попробуйте еще раз", err)
 						return err
 					}
 				}
--- go.mod
+++ go.mod
@@ -1,18 +1,18 @@
 module github.com/mrkovshik/grpc_vacancy_database_client
 
-go 1.19
+go 1.18
 
 require (
 	github.com/lib/pq v1.10.7
-	github.com/mrkovshik/grpc_vacancy_database v0.0.0-20230119141814-7de6c3f8143f
-	google.golang.org/grpc v1.52.0
+	github.com/mrkovshik/grpc_vacancy_database v0.0.0-20230125084332-772fc460122d
+	google.golang.org/grpc v1.52.1
 )
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
-	golang.org/x/net v0.4.0 // indirect
-	golang.org/x/sys v0.3.0 // indirect
-	golang.org/x/text v0.5.0 // indirect
-	google.golang.org/genproto v0.0.0-20221118155620-16455021b5e6 // indirect
+	golang.org/x/net v0.5.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/text v0.6.0 // indirect
+	google.golang.org/genproto v0.0.0-20230124163310-31e0e69b6fc2 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 )
